290cca1dde2475997b11db1422d9831e5caf6d26
sometimessquishy <sebastian.arruda06@gmail.com>
2022-11-19 19:44:26 -0500

    better documentation and general cleaning up.

--- githubpush.sh
+++ githubpush.sh
@@ -7,13 +7,15 @@
 #!/usr/bin/env bash
 #To run this go into your folder you want to push and then run the script
 #TO DO###
-#Make it work outside of home directory, make it not use cat to put at top of file
+#make it not use cat to put at top of file
 
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 
 
+
+# Pulling github token, then 256b encrypting it to be stored.
 if [[ -z "$GTOK" ]]; then
 	echo "what is your github token?"
 	read -rs GTOKEN
@@ -30,6 +32,8 @@ fi
 
 
 
+
+# Grabbing user name, and then saving it
 if [[ -z "$USR"  ]]; then
 	echo "what is your user name?"
 	read -r USR
@@ -39,12 +43,23 @@ else
 	echo "is $USR your correct username?"
 fi
 
+
+
+# Creating GPG pair to sign on uploads, then saving private key in file.
 if [[ -z "$GPG" ]]; then
 	echo "would you like to make a gpg key to sign your update things?"
+	echo "This will require you to open a web browser window"
+	echo "if yes then press enter"
 	read -r GPGYN
 
 	if [[ -z "$GPGYN" ]]; then
-		#gpg --generate-key
+		echo "do you need to create a gpg key? y for yes, enter for no."
+		read -r GPGGENKEYYY
+
+			if [[ "GPGGENKEYY" = "y" ]]; then
+		gpg --generate-key
+			else
+
 	POSSGPG=$(gpg --list-secret-keys --keyid-format=long | grep sec | awk -F'/' '{print $2}' | awk '{print $1}'); git config --global user.signingkey $POSSGPG; git config --global commit.gpgsign true
 	sed -i "1i GPGYN=$POSSGPG" $SCRIPT_DIR/githubpush.sh
 
@@ -58,9 +73,8 @@ fi
 
 
 
-#echo "What is your repo named?"
-#read -r REPO
-
+# Grabbing repo name through using just the base name of the directory you are in.
+# Will work great as long as you dont rename your repo.
 REPO=$(basename $PWD)
 
 echo "is $REPO youre repository? press n if no."
@@ -75,12 +89,17 @@ fi
 
 
 
-
+# grabbing password to decrypt your github token.
 echo "remember your password?"
 read -rs PW2
 
+UNENCGTOK=$(echo $GTOK | openssl aes-256-cbc -d -a -pass pass:$PW2 -pbkdf2)
+
+
+
+# selecting files to be pushed
 echo "do you want to add all the files in your directory ($PWD) to be edited?"
-echo "y for all files in ($PWD). If not type in file name"
+echo " >> y << for all files in ($PWD). If not type in file name"
 read -r UPDATEE
 
 	if [ "$UPDATEE" = "y" ]; then
@@ -91,15 +110,15 @@ else
 
 
 
-UNENCGTOK=$(echo $GTOK | openssl aes-256-cbc -d -a -pass pass:$PW2 -pbkdf2)
-
+# setting the commit message
 echo "what would you like your commit message to be?"
 read -r MESSAGE
 
-
-
 git commit -S -m "$MESSAGE"
 
+
+
+#and finally pushing to github
 git push https://$UNENCGTOK@github.com/$USR/$REPO.git
 
 #
